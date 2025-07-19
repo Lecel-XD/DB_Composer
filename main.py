d95d5500e956996079b601aa3bc607af61f17f81
Lecel <X_lecel_X@inbox.ru>
2025-07-20 01:37:41 +0500

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+input_name = '../Temp/fi.csv'
+output_name = '../Temp/fo.csv'
+yo_check = False
+
+class Employee:
+    def Export(self):
+        pass
+    def PrintHeader(self):
+        pass
+    def Merge(self, obj):
+        pass
+
