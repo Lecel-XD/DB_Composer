2237e8f43e145c78c6fccb11c03e9846dcf82011
Lecel <X_lecel_X@inbox.ru>
2025-07-20 21:56:55 +0500

    Added __init__

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ from csv import DictReader
 
 input_name = '../Temp/fi.csv'
 output_name = '../Temp/fo.csv'
-yo_check = False
+# yo_check = False
 
 
 class Employee:
-    identity = 1
+    def __init__(self, **kwargs):
+        self._constant = {}
+        self._subsidia = {}
+        self.premii = {}
+        self.identity = 1
+    def __eq__(self, other):
+        return self.identity == other.identity
     # в ините сделать разбиение выплат на категории (постоянные, субсидии и т.д.)
     def Export(self):
-        pass
-    def PrintHeader(self):
         pass
     def Merge(self, obj):
         pass
@@ -21,15 +25,16 @@ class Employee:
 employees = []
 indexes = {}
 counter = 0
-before_counting_rows = ['', 'Месяц начисления', 'Организация']
+before_counting_rows = ['', 'Месяц начисления', 'Организация', 'Сотрудник.Таб. номер', 'Должность', 'Подразделение', 'Сотрудник.Физическое лицо.Пол', 'Сотрудник', 'Сальдо на начало месяца']
 # экспорт должен брать первый столбец из identity, а не вычислять его заново
 
 
 
 # создание списка сотрудников без дубликатов
 with open(input_name) as fi:
-    for person in DictReader(fi, delimiter=','):
-        obj = Employee(person)
+    dictionary = DictReader(fi, delimiter=',')
+    for person in dictionary:
+        obj = Employee(**person)
         if obj.identity in indexes:
             employees[indexes[obj.identity]].Merge(obj)
         else:
@@ -40,6 +45,6 @@ with open(input_name) as fi:
 
 # экспорт данных
 with open(output_name, encoding='UTF-8') as fo:
-    Employee.PrintHeader()
+    # dictionary записать заголовки - метод в csv
     for person in employees:
         person.Export()
